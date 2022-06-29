252cc678421d750fe3be1e9e929a0e1f143d60c3
shirov.ivan101@gmail.com <shirov.ivan101@gmail.com>
2022-06-30 01:19:06 +0500

    first commit

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,9 @@
-
-
 export default class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=48dbc8be51388196d5924a35ebbdf414'
+
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -12,10 +15,10 @@ export default class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=48dbc8be51388196d5924a35ebbdf414');
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     }
 
     getCharacter = (id) => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?&apikey=48dbc8be51388196d5924a35ebbdf414`);
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
     }
 }
